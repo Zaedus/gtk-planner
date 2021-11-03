ab7dc830b716f7149c21f11015357ab778a1c146
Zaedus <nintendozaedus@gmail.com>
2021-11-03 18:21:40 -0500

    Incorperate card into section class

--- src/section.hpp
+++ src/section.hpp
@@ -1,17 +1,22 @@
 #pragma once
 
 class Project;
+class Card;
 
+#include <vector>
 #include "project.hpp"
+#include "card.hpp"
 
 class Section 
 {
 private:
-    Project *project;
     GtkBox *container;
     int position;
     std::string title;
+    std::vector<Card> cards;
     
 public:
     Section(Project *project, std::string title, int position);
+
+    Project *project;
 };
